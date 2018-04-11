2a4c7d0c263f9c87ed858729944d254830a2aae7
Pedro <pfpinheiro64@gmail.com>
2018-04-11 21:20:28 +0100

    ficha6e5

---
+++ Ficha6/Ex1/Script.js
@@ -0,0 +1,27 @@
+"use strict";
+
+
+document.getElementById('button').addEventListener('click', function () {
+    
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+            if(this.readyState == 4 && this.status== 200) {
+                let obj = JSON.parse(this.responseText);
+                for(let i = 0 ; i< obj.countries.length; i++){
+                    //Print Code
+                    let code = document.createElement('p');
+                    document.getElementById('conteudo').appendChild(code);
+                    code.innerHTML = "Code: " + obj.countries[i].code;
+                    //Print Name
+                    let name = document.createElement('p');
+                    document.getElementById('conteudo').appendChild(name);
+                    name.innerHTML = "Name: " + obj.countries[i].name;
+                }
+            }
+        };
+    xhttp.open("GET","getcountries",true);
+    xhttp.send();
+    
+
+
+});
---
+++ Ficha6/Ex2/Server.js
@@ -0,0 +1,91 @@
+﻿var http = require('http');
+var fs = require('fs');
+var path = require('path');
+
+http.createServer(function (request, response) {
+    console.log('request ', request.url);
+
+    var filePath = '.' + request.url;
+    if (request.url == '/') {
+        filePath += 'index.html';
+    }
+
+    if (filePath.includes('./action_page.php')) {
+        let url = require('url');
+        var q = url.parse(filePath, true);
+        var qdata = q.query;
+
+        let person = {
+            name: qdata.firstname,
+            last: qdata.lastname,
+            email: qdata.email,
+            codigo: qdata.codigopostal,
+            pais: qdata.pais,
+            data: qdata.dob,
+            conducao: qdata.ncarta,
+            morada: qdata.morada,
+            titulo: qdata.titulo
+        };
+
+
+
+        fs = require('fs');
+
+        fs.writeFile(qdata.firstname + qdata.lastname + ".json", JSON.stringify(person, null, 2), function (err) {
+            if (err) {
+                return console.log('Erro');
+            }
+            console.log('Done');
+        });
+
+    }
+
+
+
+
+
+
+    var extname = String(path.extname(filePath)).toLowerCase();
+    var mimeTypes = {
+        '.html': 'text/html',
+        '.js': 'text/javascript',
+        '.css': 'text/css',
+        '.json': 'application/json',
+        '.png': 'image/png',
+        '.jpg': 'image/jpg',
+        '.gif': 'image/gif',
+        '.wav': 'audio/wav',
+        '.mp4': 'video/mp4',
+        '.woff': 'application/font-woff',
+        '.ttf': 'application/font-ttf',
+        '.eot': 'application/vnd.ms-fontobject',
+        '.otf': 'application/font-otf',
+        '.svg': 'application/image/svg+xml'
+    };
+
+    var contentType = mimeTypes[extname] || 'application/octet-stream';
+
+    fs.readFile(filePath, function (error, content) {
+        if (error) {
+            console.log(error);
+            if (error.code == 'ENOENT') {
+                fs.readFile('./404.html', function (error, content) {
+                    response.writeHead(200, { 'Content-Type': contentType });
+                    response.end(content, 'utf-8');
+                });
+            }
+            else {
+                response.writeHead(500);
+                response.end('Sorry, check with the site admin for error: ' + error.code + ' ..\n');
+                response.end();
+            }
+        }
+        else {
+            response.writeHead(200, { 'Content-Type': contentType });
+            console.log(content);
+            response.end(content, 'utf-8');
+        }
+    });
+
+}).listen(8000);
+console.log('Server running at http://127.0.0.1:8125/');
